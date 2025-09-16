1c35c2b902e6206a417abc1a1c2903dd6f8d600c
Jano Soler <janosolerdiaz@gmail.com>
2025-09-16 13:23:18 +0200

    feat: current combination max size four

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,22 @@ redSquare.addEventListener("click", (e)=>{
     } 
 });*/
 const colorButtons = document.querySelectorAll(".color-square");
+const sendCombinationButton = document.getElementById("send-combination-button");
+let currentSquareCombination: NodeListOf<Element>;
 colorButtons.forEach((element)=>{
-    element.addEventListener("click", ()=>{
-        //crear nuevo elemento
-        //añadir clases del elemento clikado
-        //añadir a la sección current combination
+    element.addEventListener("click", (e)=>{
+        currentSquareCombination = document.querySelectorAll(".current-square");
+        console.log(currentSquareCombination.length);
+        if (currentSquareCombination.length<4){
+            const newCurrentCombinationButton:HTMLElement = document.createElement("div");
+            newCurrentCombinationButton.classList.add("current-square");
+            if (e.target instanceof HTMLElement){
+                e.target.classList.forEach((element)=>{
+                    newCurrentCombinationButton.classList.add(element);
+                })   
+            }
+            sendCombinationButton.insertAdjacentElement('beforebegin', newCurrentCombinationButton);
+        }
+        
     })
 })
