0e259521bda794b53da4eee6d4289feb5971b322
Jano Soler <janosolerdiaz@gmail.com>
2025-09-16 11:56:19 +0200

    inicio mastermind

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hola Mundo");
+/*const redSquare: HTMLElement = document.getElementById("red-square");
+redSquare.addEventListener("click", (e)=>{
+    if (e.target instanceof HTMLElement){
+       if (e.target.classList.contains("red")){
+        e.target.classList.replace("red", "green");
+       } else if (e.target.classList.contains("green")){
+        e.target.classList.replace("green", "red");
+       } 
+    } 
+});*/
+const colorButtons = document.querySelectorAll(".color-square");
+colorButtons.forEach((element)=>{
+    element.addEventListener("click", ()=>{
+        //crear nuevo elemento
+        //añadir clases del elemento clikado
+        //añadir a la sección current combination
+    })
+})
