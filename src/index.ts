903a3bba2a28268f98e02d99f7663e1710524079
Maikel <maikelcorreoelectronico@gmail.com>
2025-09-22 13:40:24 +0200

    inicio mastermind

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log("Hello, TypeScript!");
+const colorsButtons = document.querySelectorAll(".color-square");
+colorsButtons.forEach((element) => {
+    element.addEventListener("click", () => {
+        //crear nuevo elemento
+        //añadir clases de elemento clikado
+        //añadir a la sección current combination
+    })
+})
+
+
+
+
+
+
+
+
+const redSquare = document.getElementById("red-square");
+redSquare.addEventListener("click", (e) => {
+    if (e.target instanceof HTMLElement){
+        if (e.target.classList.contains("red")) {
+            e.target.classList.replace("red", "green");
+        } else if (e.target.classList.contains("green")) {
+            e.target.classList.replace("green", "red");
+        }
+    }
+})
+
+const botonCambia: HTMLElement = document.getElementById("botonCambia")
+
+document.getElementById("botonCambia").addEventListener("click", () => {
+    document.getElementById("cabecera").innerText += " con VanillaJS"
+
+});
+
+document.getElementById("botonCambia").addEventListener("mouseover", () => {
+    document.getElementById("cabecera").classList.add("roja");
+})
+
+
